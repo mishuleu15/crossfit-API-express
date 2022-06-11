76c451af2b61f99f44cc21511abde567b8bbecc4
Catalin Mihai <mishu_leu15@yahoo.com>
2022-06-11 17:43:51 +0300

    heroku test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const path = require('path');
+
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
 const v1AuthRouter = require('./v1/routes/authRoutes');
 const v1UserRouter = require('./v1/routes/userRoutes');
@@ -13,18 +18,20 @@ const PORT = process.env.PORT || 3001;
 // db and authenticateUser
 const connectDB = require('../src/db/connect');
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(cors());
-
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use('/api/v1/workouts', v1WorkoutRouter);
 app.use('/api/v1/auth', v1AuthRouter);
 app.use('/api/v1/user', v1UserRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to CrossFit API');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 const start = async () => {
--- src/v1/routes/authRoutes.js
+++ src/v1/routes/authRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const rateLimiter = require('express-rate-limit');
 
 const { register, signIn } = require('../../controllers/authController');
 
 const router = express.Router();
 
-router.post('/', signIn);
-router.post('/register', register);
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
+router.post('/', apiLimiter, signIn);
+router.post('/register', apiLimiter, register);
 
 module.exports = router;
